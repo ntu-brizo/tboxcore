3e7367da00adec03df342cc998e6168172cd2b95
VensonLiou <48060449+VensonLiou@users.noreply.github.com>
2019-09-23 11:58:35 +0800

    Update Meta

--- meta.go
+++ meta.go
@@ -1,14 +1,14 @@
 package tboxcore
 
 type Meta struct {
-	size                 int64 `json:"size"`
-	isDirectory          bool `json:"isDirectory"`
-	isRegularFile        bool `json:"isRegularFile"`
-	isSimbolicLink       bool `json:"isSimbolicLink"`
-	isHidden             bool `json:"isHidden"`
-	lastModifyTime       int64 `json:"lastModifyTime"`
-	owner                string `json:"owner"`
-	posixFilePermissions string `json:"posixFilePermissions"`
-	mimeType             string `json:"mimeType"`
-	notes                string `json:"notes"`
+	Size                 int64 `json:"Size"`
+	IsDirectory          bool `json:"IsDirectory"`
+	IsRegularFile        bool `json:"IsRegularFile"`
+	IsSimbolicLink       bool `json:"IsSimbolicLink"`
+	IsHidden             bool `json:"IsHidden"`
+	LastModifyTime       int64 `json:"LastModifyTime"`
+	Owner                string `json:"Owner"`
+	PosixFilePermissions string `json:"PosixFilePermissions"`
+	MimeType             string `json:"MimeType"`
+	Notes                string `json:"Notes"`
 }
